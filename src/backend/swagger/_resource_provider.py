1afc918beb96f37400e89fe3527f7c0bb5bc32c5
Kai Ru <kairu@microsoft.com>
2021-07-19 11:09:45 +0800

    support to parse yaml input-files in readme

--- src/backend/swagger/_resource_provider.py
+++ src/backend/swagger/_resource_provider.py
@@ -1,4 +1,10 @@
 from ._utils import map_path_2_repo
+import re
+import yaml
+import os
+import logging
+
+logger = logging.getLogger('backend')
 
 
 class ResourceProvider:
@@ -10,5 +16,57 @@ class ResourceProvider:
         self._swagger_module = swagger_module
 
         if readme_path is None:
-            print(f"MissReadmeFile: {self._swagger_module}/{self._name}: {map_path_2_repo(path)}")
+            logger.warning(f"MissReadmeFile: {self._swagger_module}/{self._name}: {map_path_2_repo(path)}")
+        self._tags = None
+
+    @property
+    def tags(self):
+        if self._tags is None:
+            self._tags = self._parse_readme_input_file_tags()
+        return self._tags
+
+    def _parse_readme_input_file_tags(self):
+        tags = {}
+        if self._readme_path is None:
+            return tags
+
+        with open(self._readme_path, 'r', encoding='utf-8') as f:
+            readme = f.read()
+
+        re_yaml = re.compile(
+            r'```\s*yaml\s*(.*\$\(\s*tag\s*\)\s*==\s*[\'"]\s*(.*)\s*[\'"].*)?\n((((?!```).)*\n)*)```\s*\n',
+            flags=re.MULTILINE)
+        for piece in re_yaml.finditer(readme):
+            flags = piece[1]
+            yaml_body = piece[3]
+            if 'input-file' not in yaml_body:
+                continue
+
+            try:
+                body = yaml.safe_load(yaml_body)
+            except yaml.YAMLError as err:
+                logger.error(f'ParseYamlFailed: {self._readme_path} {flags}: {err}')
+                continue
+
+            files = []
+            for file_path in body['input-file']:
+                file_path = file_path.replace('$(this-folder)/', '')
+                file_path = os.path.join(os.path.dirname(self._readme_path), *file_path.split('/'))
+                if not os.path.isfile(file_path):
+                    logger.warning(f'FileNotExist: {file_path}')
+                    continue
+                files.append(file_path)
+
+            tag = piece[2]
+            if tag is None:
+                tag = ''
+            tag = tag.strip()
+            if tag not in tags:
+                tags[tag] = set()
+            tags[tag].union(files)
+        return tags
+
+
+
+
 
