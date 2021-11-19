e575dc844387f7b67412684a62708ef8483a1389
necusjz <necusjz@gmail.com>
2021-11-19 14:54:52 +0800

    Update src/backend/aazdev/operations/config.py
    Co-authored-by: kai ru <69238381+kairu-ms@users.noreply.github.com>

--- src/backend/aazdev/operations/config.py
+++ src/backend/aazdev/operations/config.py
@@ -12,6 +12,7 @@ from swagger.controller.command_generator import CommandGenerator
 
 def generate_config(swagger_path, config_path, module_name, resource_id, api_version):
     generator = CommandGenerator(module_name="(MgmtPlane)/" + module_name)
+    resource_id = swagger_path_to_resource_id(resource_id)
     cmd_resource = CMDResource({"id": resource_id, "version": api_version})
     resources = generator.load_resources([cmd_resource])
     command_group = generator.create_draft_command_group(resources[resource_id])
