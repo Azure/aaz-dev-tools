f7d24ccdb0081a80d883b20ea22c2865ce1692c3
necusjz <necusjz@gmail.com>
2021-12-03 15:24:15 +0800

    Refactoring

--- src/backend/aazdev/operations/config.py
+++ src/backend/aazdev/operations/config.py
@@ -4,15 +4,16 @@
 # license information.
 # -----------------------------------------------------------------------------
 
-import re
-import html
-import inflect
+from schematics.types import ListType, ModelType
 
-from lxml.builder import ElementMaker
-from lxml.etree import Element, tostring
-
-from command.model.configuration import CMDResource
+from .xml import XMLModel
+from command.model.configuration import CMDResource, CMDCommandGroup
 from swagger.controller.command_generator import CommandGenerator
+
+
+class CodeGen(XMLModel):
+    resource = ListType(ModelType(CMDResource), min_size=1)
+    commandGroup = ModelType(CMDCommandGroup)
 
 
 def generate_config(swagger_path, config_path, module_name, resource_id, api_version):
@@ -20,50 +21,8 @@ def generate_config(swagger_path, config_path, module_name, resource_id, api_ver
     cmd_resource = CMDResource({"id": resource_id, "version": api_version})
     resources = generator.load_resources([cmd_resource])
     command_group = generator.create_draft_command_group(resources[resource_id])
-    # structurally build configuration via xml
-    root = Element("CodeGen", version="2.0")
-    root.append(_build_xml("resource", cmd_resource.to_primitive()))
-    root.append(_build_xml("command_group", command_group.to_primitive()))
 
+    model = CodeGen({"resource": [cmd_resource], "commandGroup": command_group})
     with open(config_path, "w") as fp:
-        fp.write(
-            html.unescape(
-                tostring(root, xml_declaration=True,  pretty_print=True, encoding="utf-8").decode()
-            )
-        )
+        fp.write(model.to_xml())
     return "Done."
-
-
-def _build_xml(root, primitive, parent=None):
-    linker = ElementMaker()
-    if parent is None:
-        parent = getattr(linker, root)()
-    # normalize the name of element tag
-    if curr_tag := _inflect_engine.singular_noun(parent.tag):
-        parent.tag = curr_tag
-    camel_pattern = re.compile(r"(?<!^)(?=[A-Z])")
-    parent.tag = camel_pattern.sub("_", parent.tag).lower()
-
-    for key, val in primitive.items():
-        _primitive_to_xml(root, key, val, parent)
-    return parent
-
-
-def _primitive_to_xml(root, key, val, parent):
-    linker = ElementMaker()
-    if isinstance(val, dict):
-        _parent = getattr(linker, key)()
-        parent.append(_build_xml(root, val, _parent))
-    elif isinstance(val, list):
-        for _val in val:
-            _primitive_to_xml(root, key, _val, parent)
-    else:
-        # sort when one key has multiple values
-        if prev := parent.get(key):
-            curr = " ".join(sorted(f"{prev} {val}".split(), reverse=True))
-            parent.set(key, curr)
-        else:
-            parent.set(key, str(val))
-
-
-_inflect_engine = inflect.engine()
