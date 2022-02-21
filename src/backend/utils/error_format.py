14145eaec1228ed4bdc8e3599416b7814d936515
Kai Ru <kairu@microsoft.com>
2022-02-21 14:21:02 +0800

    support 'innerError' property

--- src/backend/utils/error_format.py
+++ src/backend/utils/error_format.py
@@ -2,35 +2,40 @@
 
 class AAZErrorFormatEnum:
 
-    ODataV4Format = "ODataV4Format"  # registered in n azure.cli.core.aaz._error_format
-    MgmtErrorFormat = "MgmtErrorFormat"
+    ODataV4Format = "ODataV4Format"  # registered in azure.cli.core.aaz._error_format
+    MgmtErrorFormat = "MgmtErrorFormat"  # registered in azure.cli.core.aaz._error_format
 
     @classmethod
     def classify_error_format(cls, builder, schema):
         from command.model.configuration import CMDObjectSchemaBase, CMDObjectSchema, CMDClsSchemaBase
         if isinstance(schema, CMDClsSchemaBase):
             schema = builder.get_cls_definition_model(schema)
-        if not isinstance(schema, CMDObjectSchemaBase):
+        if not isinstance(schema, CMDObjectSchemaBase) or not schema.props:
             return None
 
         props = {}
         for prop in schema.props:
             props[prop.name] = prop
-        if "error" not in props:
-            return None
 
-        schema = props['error']
-        if isinstance(schema, CMDClsSchemaBase):
-            schema = builder.get_cls_definition_model(schema)
-        if not isinstance(schema, CMDObjectSchema):
-            return None
+        if "error" in props:
+            schema = props['error']
+            if isinstance(schema, CMDClsSchemaBase):
+                schema = builder.get_cls_definition_model(schema)
+            if not isinstance(schema, CMDObjectSchema):
+                return None
 
         prop_keys = set()
         for prop in schema.props:
             prop_keys.add(prop.name)
-        if prop_keys == {"code", "message", "target", "details"}:
+
+        if not ("code" in prop_keys and "message" in prop_keys):
+            # code and message is required
+            return None
+
+        prop_keys.difference_update({"code", "message", "target", "details", "innerError", "innererror"})  # some api use "innerError" instead of "innererror"
+        if len(prop_keys) == 0:
             return cls.ODataV4Format
-        if prop_keys == {"code", "message", "target", "details", "additionalInfo"}:
+        if prop_keys == {"additionalInfo"}:
             return cls.MgmtErrorFormat
 
         return None
