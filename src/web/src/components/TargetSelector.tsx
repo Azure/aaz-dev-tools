6c9c3c5e94383e5eff9acad303f706e70d2489fc
necusjz <necusjz@gmail.com>
2022-03-07 12:51:12 +0800

    refine modal header

--- src/web/src/components/TargetSelector.tsx
+++ src/web/src/components/TargetSelector.tsx
@@ -1,26 +1,41 @@
 import {Component} from "react";
-import {Modal, Button} from "react-bootstrap";
+import {Modal, Navbar, Row, Col, Button} from "react-bootstrap";
+import "./TargetSelector.css";
 
 
 export default class TargetSelector extends Component {
 
   SelectTarget = () => {
     return <div>
-      <Modal show="true" centered>
-        <Modal.Header>
-          <Modal.Title>
-            Please select your target
+      <Modal show="true" dialogClassName="modal-width" centered>
+        <Modal.Header className="modal-header" closeButton>
+          <Modal.Title className="model-title">
+            Welcome to CodeGen 2.0
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div className="d-grid gap-2">
-            <Button variant="secondary" size="lg">
-              Azure CLI
-            </Button>
-            <Button variant="secondary" size="lg">
-              Azure CLI Extension
-            </Button>
-          </div>
+          <Row>
+            <Col sm={4}>
+              <div className="d-grid gap-2">
+                <Button variant="secondary" size="lg">
+                  Azure CLI
+                </Button>
+                <Button variant="secondary" size="lg">
+                  Azure CLI Extension
+                </Button>
+              </div>
+            </Col>
+            <Col sm={8}>
+              <div className="d-grid gap-2">
+                <Button variant="secondary" size="lg">
+                  Azure CLI
+                </Button>
+                <Button variant="secondary" size="lg">
+                  Azure CLI Extension
+                </Button>
+              </div>
+            </Col>
+          </Row>
         </Modal.Body>
       </Modal>
     </div>
