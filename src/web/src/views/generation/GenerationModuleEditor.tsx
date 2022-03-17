0ab4237883a52ceb9bdd0ad90d89289c7ffef527
necusjz <necusjz@gmail.com>
2022-03-17 17:02:17 +0800

    add profile selector

--- src/web/src/views/generation/GenerationModuleEditor.tsx
+++ src/web/src/views/generation/GenerationModuleEditor.tsx
@@ -1,64 +1,186 @@
-import * as React from 'react';
-import {Typography, Box, Link } from '@mui/material';
-import { styled } from '@mui/material/styles';
-import { useParams } from 'react-router';
+import * as React from "react";
+import { Typography, Box, Link, Tabs, Tab } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import { useParams } from "react-router";
 import GenerationModuleEditorToolBar from "./GenerationModuleEditorToolBar";
+import ModuleProfileSelector from "./ModuleProfileSelector";
+import axios from "axios";
+import EditorPageLayout from "../../components/EditorPageLayout";
 
 const TopPadding = styled(Box)(({ theme }) => ({
-    [theme.breakpoints.up('sm')]: {
-        height: '12vh',
-    },
+  [theme.breakpoints.up("sm")]: {
+    height: "12vh",
+  },
 }));
 
 const MiddlePadding = styled(Box)(({ theme }) => ({
-    height: '6vh'
+  height: "6vh",
 }));
 
+type RegisterType = {
+  stage: string;
+};
+
+type ResourceType = {
+  id: string;
+  plane: string;
+  version: string;
+};
+
+type ExampleTye = {
+  commands: string[];
+  name: string;
+};
+
+type HelpType = {
+  short: string;
+  examples?: ExampleTye[];
+};
+
+type LeafType = {
+  help: HelpType;
+  names: string[];
+  registerInfo: RegisterType;
+  resources: ResourceType[];
+  stage: string;
+  version: string;
+};
+
+type Leaves = {
+  [name: string]: LeafType;
+};
+
+type NodeType = {
+  help: HelpType;
+  names: string[];
+  registerInfo: RegisterType;
+  commandGroups?: Nodes;
+  commands?: Leaves;
+};
+
+type Nodes = {
+  [name: string]: NodeType;
+};
+
 interface GenerationModuleEditorProps {
-    params: {
-        repoName: string,
-        moduleName: string,
-    }
+  params: {
+    repoName: string;
+    moduleName: string;
+  };
 }
 
 interface GenerationModuleEditorState {
-    moduleName: string,
+  repoName: string;
+  moduleName: string;
+  toBeGenerated: Nodes;
+  profileIndex: number;
+  profiles: string[];
 }
 
+class GenerationModuleEditor extends React.Component<
+  GenerationModuleEditorProps,
+  GenerationModuleEditorState
+> {
+  constructor(props: GenerationModuleEditorProps) {
+    super(props);
+    this.state = {
+      repoName: this.props.params.repoName,
+      moduleName: this.props.params.moduleName,
+      toBeGenerated: {},
+      profileIndex: 0,
+      profiles: [],
+    };
+  }
 
-class GenerationModuleEditor extends React.Component<GenerationModuleEditorProps, GenerationModuleEditorState> {
-    
-    constructor(props: GenerationModuleEditorProps) {
-        super(props);
-        this.state = {
-            moduleName: this.props.params.moduleName,
-        }
-    }
+  componentDidMount() {
+    this.loadProfiles();
+    this.loadCommandsModels();
+    this.loadLocalCommands();
+  }
 
-    handleBackToHomepage = () => {
-        window.location.href = `/?#/workspace`
-    }
+  loadProfiles = () => {
+    axios
+      .get("/CLI/Az/Profiles")
+      .then((res) => {
+        this.setState({ profiles: res.data });
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
+  };
 
-    handleGenerate = () => {
+  loadCommandsModels = () => {};
 
-    }
+  loadLocalCommands = () => {
+    axios
+      .get(`/CLI/Az/Main/Modules/${this.state.moduleName}`)
+      .then((res) => {
+        this.setState({ toBeGenerated: res.data["profiles"] });
+      })
+      .catch((err) => {
+        console.error(err.response);
+      });
+  };
 
-    render() {
-        const { moduleName} = this.state
-        return (
-            <React.Fragment>
-                <GenerationModuleEditorToolBar moduleName={moduleName} onHomePage={this.handleBackToHomepage} onGenerate={this.handleGenerate}>
+  handleBackToHomepage = () => {
+    window.location.href = `/?#/generation`;
+  };
 
-                </GenerationModuleEditorToolBar>
-            </React.Fragment>
-        )
-    }
+  handleGenerate = () => {
+    axios
+      .put(`/CLI/Az/${this.state.repoName}/Modules/${this.state.moduleName}`, {
+        profiles: this.state.toBeGenerated,
+      })
+      .then(() => {})
+      .catch((err) => {
+        console.error(err.response);
+      });
+  };
+
+  handleProfileChange = (event: React.SyntheticEvent, newValue: number) => {
+    this.setState({ profileIndex: newValue });
+  };
+
+  render() {
+    const { moduleName } = this.state;
+    return (
+      <React.Fragment>
+        <GenerationModuleEditorToolBar
+          moduleName={moduleName}
+          onHomePage={this.handleBackToHomepage}
+          onGenerate={this.handleGenerate}
+        >
+
+        </GenerationModuleEditorToolBar>
+        <EditorPageLayout>
+          <Box
+            sx={{
+              flexShrink: 0,
+              width: 250,
+              flexDirection: "column",
+              display: "flex",
+              alignItems: "stretch",
+              justifyContent: "flex-start",
+              marginRight: "3vh",
+            }}
+          >
+            <ModuleProfileSelector
+                value={this.state.profileIndex}
+              profiles={this.state.profiles}
+              onChange={this.handleProfileChange}
+            >
+
+            </ModuleProfileSelector>
+          </Box>
+        </EditorPageLayout>
+      </React.Fragment>
+    );
+  }
 }
-
 
 const GenerationModuleEditorWrapper = (props: any) => {
-    const params = useParams()
-    return <GenerationModuleEditor params={params} {...props} />
-}
+  const params = useParams();
+  return <GenerationModuleEditor params={params} {...props} />;
+};
 
 export { GenerationModuleEditorWrapper as GenerationModuleEditor };
---
+++ src/web/src/views/generation/ModuleProfileSelector.tsx
@@ -0,0 +1,48 @@
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Box from "@mui/material/Box";
+
+function a11yProps(index: number) {
+  return {
+    id: `vertical-tab-${index}`,
+    "aria-controls": `vertical-tabpanel-${index}`,
+  };
+}
+
+interface ModuleProfileSelectorProps {
+  value: number;
+  profiles: string[];
+  onChange: (event: React.SyntheticEvent, newValue: number) => void;
+}
+
+class ModuleProfileSelector extends React.Component<ModuleProfileSelectorProps> {
+  render() {
+    const { value, profiles, onChange } = this.props;
+    return (
+      <Box
+        sx={{
+          flexGrow: 1,
+          bgcolor: "background.paper",
+          display: "flex",
+          height: 224,
+        }}
+      >
+        <Tabs
+          orientation="vertical"
+          variant="scrollable"
+          value={value}
+          onChange={onChange}
+          aria-label="Vertical tabs example"
+          sx={{ borderRight: 1, borderColor: "divider" }}
+        >
+          {profiles.map((profile, idx) => {
+            return <Tab key={idx} label={profile} {...a11yProps(idx)} />;
+          })}
+        </Tabs>
+      </Box>
+    );
+  }
+}
+
+export default ModuleProfileSelector;
