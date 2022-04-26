2f30dd047a89b1f1de85834a317acd68d2a913f5
necusjz <necusjz@gmail.com>
2022-04-26 15:29:00 +0800

    change entry point

--- README.md
+++
@@ -1,3 +0,0 @@
-# Microsoft Atomic Azure CLI Dev Tools (aazdev)
-
-The `aazdev` tool is designed to generate atomic azure cli commands from OpenAPI specifications.
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-import os
 from setuptools import find_packages, setup
 
 
 def read_requirements(filename: str):
-    with open(os.path.join(curr_path, filename)) as requirements_file:
+    with open(filename) as requirements_file:
         import re
 
         def fix_url_dependencies(req: str) -> str:
@@ -28,13 +27,12 @@ def read_requirements(filename: str):
 # version.py defines the VERSION and VERSION_SHORT variables.
 # We use exec here, so we don't import cached_path whilst setting up.
 VERSION = {}  # type: ignore
-with open(os.path.join(os.getcwd(), "scripts", "version.py"), "r") as version_file:
+with open("scripts/version.py", "r") as version_file:
     exec(version_file.read(), VERSION)
 
-curr_path = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(curr_path, "README.rst"), "r", encoding="utf-8") as fp:
+with open("README.rst", "r", encoding="utf-8") as fp:
     README = fp.read()
-with open(os.path.join(curr_path, "HISTORY.rst"), "r", encoding="utf-8") as fp:
+with open("HISTORY.rst", "r", encoding="utf-8") as fp:
     HISTORY = fp.read()
 
 setup(
@@ -53,20 +51,22 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.10"
     ],
     keywords="azure",
     url="https://github.com/kairu-ms/aaz-dev-tools",
     author="Microsoft Corporation",
     author_email="azpycli@microsoft.com",
     license="MIT",
+    package_dir={"": "src"},
     packages=find_packages(
+        where="src",
         exclude=["*.tests", "*.tests.*", "tests.*", "tests"],
     ),
     include_package_data=True,
     install_requires=read_requirements("requirements.txt"),
     python_requires=">=3.6",
     entry_points={
-        "console_scripts": ["aazdev=aazdev.main:main"]
-    },
+        "console_scripts": ["aaz-dev=backend.aazdev.main:main"]
+    }
 )
