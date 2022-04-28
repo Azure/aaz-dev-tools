31209b2a3122d33941a7dc9ffd2b5d22a4758d09
necusjz <necusjz@gmail.com>
2022-04-28 10:14:41 +0800

    make sources root

--- src/backend/__init__.py
+++ src/backend/__init__.py
@@ -3,3 +3,9 @@
 # Licensed under the MIT License. See License.txt in the project root for
 # license information.
 # -----------------------------------------------------------------------------
+
+import sys
+from os.path import abspath, dirname, join
+
+# mark sources root
+sys.path.insert(0, join(abspath(dirname(__file__))))
