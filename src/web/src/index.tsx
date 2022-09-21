0d80dc305380cb0ff75e99dda9a4c6418fe0c8ca
kai ru <69238381+kairu-ms@users.noreply.github.com>
2022-09-21 11:31:25 +0800

    remove unused dependencies

--- src/web/src/index.tsx
+++ src/web/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter, Routes, Route } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import HomePage from './views/home/HomePage';
