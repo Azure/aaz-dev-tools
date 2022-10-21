b1b580ce68f9dc4829de2f4702ff0257fc29db23
AllyW <AllyW@users.noreply.github.com>
2022-10-21 15:49:23 +0800

    add swagger reloader resource filter (#146)
    * add swagger reloader resource filter

--- src/web/src/views/workspace/WSEditorSwaggerPicker.tsx
+++ src/web/src/views/workspace/WSEditorSwaggerPicker.tsx
@@ -45,6 +45,7 @@ interface WSEditorSwaggerPickerState {
     updateOptions: string[],
     updateOption: string,
     invalidText?: string,
+    filterText: string,
 }
 
 type ResourceVersionOperations = {
@@ -121,6 +122,7 @@ class WSEditorSwaggerPicker extends React.Component<WSEditorSwaggerPickerProps, 
             selectedVersion: null,
             updateOptions: UpdateOptions,
             updateOption: UpdateOptions[0],
+            filterText: "",
         }
     }
 
@@ -492,8 +494,7 @@ class WSEditorSwaggerPicker extends React.Component<WSEditorSwaggerPickerProps, 
     }
 
     render() {
-        const { selectedResources, existingResources, resourceOptions, resourceMap, selectedVersion, selectedModule, selectedResourceInheritanceAAZVersionMap } = this.state;
-
+        const { selectedResources, existingResources, resourceOptions, resourceMap, selectedVersion, selectedModule, selectedResourceInheritanceAAZVersionMap, filterText } = this.state;
         return (
             <React.Fragment>
                 <AppBar sx={{ position: "fixed" }}>
@@ -602,12 +603,17 @@ class WSEditorSwaggerPicker extends React.Component<WSEditorSwaggerPickerProps, 
                                             }}
                                         />
                                     </ListItemButton>
-                                    {/* <SortIcon sx={{ ml: 2, mr: 2 }} />
                                     <InputBase
                                         sx={{ flex: 1 }}
-                                        placeholder="Filter by keywords."
-                                        inputProps={{ 'aria-label': 'Filter by keywords.' }}
-                                    /> */}
+                                        placeholder="Filter by keywords"
+                                        inputProps={{ 'aria-label': 'Filter by keywords' }}
+                                        value={filterText}
+                                        onChange={(event: any) => {
+                                            this.setState({
+                                                filterText: event.target.value,
+                                                })
+                                        }}
+                                    />
                                 </Paper>
                             </Box>
                         </ListSubheader>}
@@ -615,7 +621,11 @@ class WSEditorSwaggerPicker extends React.Component<WSEditorSwaggerPickerProps, 
 
                         {resourceOptions.length > 0 && <Paper sx={{ ml: 2, mr: 2 }} variant="outlined" square>
                             {resourceOptions.filter((option) => {
-                                return option;
+                                if (filterText.trim().length > 0) {
+                                    return option.id.indexOf(filterText.trim()) > -1;
+                                }else{
+                                    return true;
+                                }
                             }).map((option) => {
                                 const labelId = `resource-${option.id}`;
                                 const selected = selectedResources.has(option.id);
