05b6677ff3650ae5c40188c163f76188dedfefd4
AllyWang <liwang1025@gmail.com>
2022-12-06 10:27:15 +0800

    add multiple modules

--- src/aaz_dev/cli/api/portal.py
+++ src/aaz_dev/cli/api/portal.py
@@ -37,57 +37,57 @@ bp = Blueprint('portal', __name__, url_prefix='/AAZ/Portal')
 )
 @click.option(
     "--module", '-m',
-    required=True,
-    help="Name of the module to generate."
+    required=True, multiple=True,
+    help="Name of the module to generate seperated."
 )
 def generate_module_command_portal(module):
     from cli.controller.az_module_manager import AzMainManager, AzExtensionManager
     from command.controller.specs_manager import AAZSpecsManager
     az_main_manager = AzMainManager()
     az_ext_manager = AzExtensionManager()
-    if az_main_manager.has_module(module):
-        cli_module = az_main_manager.load_module(module)
-        registered_cmds = az_main_manager.find_module_cmd_registered(cli_module['profiles']['latest'])
-        print("Input module: {0} in cli".format(module))
-    elif az_ext_manager.has_module(module):
-        cli_module = az_ext_manager.load_module(module)
-        registered_cmds = az_main_manager.find_module_cmd_registered(cli_module['profiles']['latest'])
-        print("Input module: {0} in cli extension".format(module))
-    else:
-        raise ValueError("Invalid input module: {0}, please check".format(module))
     aaz_spec_manager = AAZSpecsManager()
     root = aaz_spec_manager.find_command_group()
     if not root:
-        return "Command group not exist"
+        return "Command group spec root not exist"
     portal_cli_generator = PortalCliGenerator()
-    cmd_portal_list = []
-    for cmd_name_version in registered_cmds:
-        # cmd_name_version = ['monitor', 'diagnostic-setting', 'list', '2021-05-01-preview']
-        node_names = cmd_name_version[:-2]
-        leaf_name = cmd_name_version[-2]
-        registered_version = cmd_name_version[-1]
-        leaf = aaz_spec_manager.find_command(*node_names, leaf_name)
-        if not leaf or not leaf.versions:
-            print("Command group: " + " ".join(node_names) + " not exist")
-            continue
-        if not leaf.versions:
-            print("Command group: " + " ".join(node_names) + " version not exist")
-            continue
-        target_version = None
-        for v in (leaf.versions or []):
-            if v.name == registered_version:
-                target_version = v
-                break
-        if not target_version:
-            print("Command: " + " ".join(node_names) + " version not exist")
-            continue
-        print("Generating portal config of [ az {0} ] with registered version {1}".format(" ".join(cmd_name_version[:-1]),
-                                                                                          registered_version))
-        cfg_reader = aaz_spec_manager.load_resource_cfg_reader_by_command_with_version(leaf, version=target_version)
-        cmd_cfg = cfg_reader.find_command(*leaf.names)
-        cmd_portal_info = portal_cli_generator.generate_command_portal_raw(cmd_cfg, leaf, target_version)
-        if cmd_portal_info:
-            cmd_portal_list.append(cmd_portal_info)
-
-    portal_cli_generator.generate_cmds_portal(cmd_portal_list)
+    for mod in module:
+        if az_main_manager.has_module(mod):
+            cli_module = az_main_manager.load_module(mod)
+            registered_cmds = az_main_manager.find_module_cmd_registered(cli_module['profiles']['latest'])
+            print("Input module: {0} in cli".format(mod))
+        elif az_ext_manager.has_module(mod):
+            cli_module = az_ext_manager.load_module(mod)
+            registered_cmds = az_main_manager.find_module_cmd_registered(cli_module['profiles']['latest'])
+            print("Input module: {0} in cli extension".format(mod))
+        else:
+            raise ValueError("Invalid input module: {0}, please check".format(mod))
+        cmd_portal_list = []
+        for cmd_name_version in registered_cmds:
+            # cmd_name_version = ['monitor', 'diagnostic-setting', 'list', '2021-05-01-preview']
+            node_names = cmd_name_version[:-2]
+            leaf_name = cmd_name_version[-2]
+            registered_version = cmd_name_version[-1]
+            leaf = aaz_spec_manager.find_command(*node_names, leaf_name)
+            if not leaf or not leaf.versions:
+                print("Command group: " + " ".join(node_names) + " not exist")
+                continue
+            if not leaf.versions:
+                print("Command group: " + " ".join(node_names) + " version not exist")
+                continue
+            target_version = None
+            for v in (leaf.versions or []):
+                if v.name == registered_version:
+                    target_version = v
+                    break
+            if not target_version:
+                print("Command: " + " ".join(node_names) + " version not exist")
+                continue
+            print("Generating portal config of [ az {0} ] with registered version {1}".format(" ".join(cmd_name_version[:-1]),
+                                                                                              registered_version))
+            cfg_reader = aaz_spec_manager.load_resource_cfg_reader_by_command_with_version(leaf, version=target_version)
+            cmd_cfg = cfg_reader.find_command(*leaf.names)
+            cmd_portal_info = portal_cli_generator.generate_command_portal_raw(cmd_cfg, leaf, target_version)
+            if cmd_portal_info:
+                cmd_portal_list.append(cmd_portal_info)
+        portal_cli_generator.generate_cmds_portal(cmd_portal_list)
     print("done")
