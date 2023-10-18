3aa4d5b25ee38f6e9e12c2260db31a5b76477a3b
Kai Ru <kairu@microsoft.com>
2023-10-18 18:36:12 +0800

    Defined aaz specs client configration

--- src/aaz_dev/command/model/configuration/_utils.py
+++ src/aaz_dev/command/model/configuration/_utils.py
@@ -10,6 +10,7 @@ class CMDArgBuildPrefix:
     Query = '$Query'
     Header = '$Header'
     Path = '$Path'
+    Endpoint = '$Endpoint'
 
 
 DEFAULT_CONFIRMATION_PROMPT = "Are you sure you want to perform this operation?"
