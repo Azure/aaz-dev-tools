c36d59f1f2b324cf076ae96f02e7dfae261b573d
Kai Ru <kairu@microsoft.com>
2023-10-20 11:48:06 +0800

    Implement Client config model

--- src/aaz_dev/command/model/configuration/_utils.py
+++ src/aaz_dev/command/model/configuration/_utils.py
@@ -10,7 +10,7 @@ class CMDArgBuildPrefix:
     Query = '$Query'
     Header = '$Header'
     Path = '$Path'
-    Endpoint = '$Endpoint'
+    ClientEndpoint = '$Client.Endpoint'
 
 
 DEFAULT_CONFIRMATION_PROMPT = "Are you sure you want to perform this operation?"
