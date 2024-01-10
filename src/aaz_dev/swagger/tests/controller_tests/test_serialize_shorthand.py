3e06b79cda1b5e5e89d4d20acd227a447a17d0e6
necusjz <necusjz@gmail.com>
2024-01-10 15:07:12 +0800

    test: add case for shorthand syntax

---
+++ src/aaz_dev/swagger/tests/controller_tests/test_serialize_shorthand.py
@@ -0,0 +1,34 @@
+from swagger.controller._shorthand import serialize
+
+
+def test_nested():
+    obj = {
+        "microsoft-azure-monitor-single-resource-multiple-metric-criteria": {
+            "all-of": [{
+                "name": "High_CPU_80",
+                "metric-name": "Time",
+                "dimensions": [],
+                "operator": "GreaterThan",
+                "threshold": 80.5,
+                "time-aggregation": "Average"
+            }]
+        }
+    }
+
+    assert serialize(obj) == '"{microsoft-azure-monitor-single-resource-multiple-metric-criteria:{all-of:[{name:High_CPU_80,metric-name:Time,dimensions:[],operator:GreaterThan,threshold:80.5,time-aggregation:Average}]}}"'
+
+
+def test_with_single_quote():
+    obj = {
+        "name": "monitor's"
+    }
+
+    assert serialize(obj) == '"{name:monitor\'/s}"'
+
+
+def test_with_special_characters():
+    obj = {
+        "name": "monitor metric"
+    }
+
+    assert serialize(obj) == '"{name:\'monitor metric\'}"'
