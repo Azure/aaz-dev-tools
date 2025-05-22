0c698a9e2b9ad671e4e9ac163da7565fe66677a2
necusjz <necusjz@gmail.com>
2025-05-22 19:26:07 +1000

    feat: support patch when reload

--- src/aaz_dev/command/controller/workspace_manager.py
+++ src/aaz_dev/command/controller/workspace_manager.py
@@ -824,13 +824,14 @@ class WorkspaceManager:
                 options['update_by'] = update_by
             new_cfg_editor = self._build_draft_cfg_editor(command_generator, resource, options)
             new_cfg_editor.inherit_modification(cfg_editor)
-            temp_generic_update_cmd = None
-            if update_cmd_info and update_by == "PatchOnly":
-                temp_cfg_editor = self._build_draft_cfg_editor(command_generator, resource, {"update_by": "GenericOnly", "methods": ('get', 'put')})
+            temp_update_cmd = None
+            if update_cmd_info and update_by == "PatchOnly" and cfg_editor.find_identity_schema_in_command(update_cmd_info[1]):
+                temp_cfg_editor = self._build_draft_cfg_editor(command_generator, resource, {"update_by": "PatchOnly", "methods": ('get', 'patch'), "is_identity": True})
                 temp_update_cmd_info = temp_cfg_editor.get_update_cmd(resource_id)
                 if temp_update_cmd_info:
-                    _, temp_generic_update_cmd, _ = temp_update_cmd_info
-            new_cfg_editor.build_identity_subresource(resource_id, temp_generic_update_cmd)
+                    _, temp_update_cmd, _ = temp_update_cmd_info
+
+            new_cfg_editor.build_identity_subresource(resource_id, temp_update_cmd)
             new_cfg_editors.append(new_cfg_editor)
 
         # remove old cfg editor
