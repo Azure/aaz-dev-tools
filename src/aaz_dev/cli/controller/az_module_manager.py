eaca5fa090bc1e4949e82c34dc36aa406055cb5a
necusjz <necusjz@gmail.com>
2025-09-29 13:36:58 +1000

    fix: empty directories when git revert

--- src/aaz_dev/cli/controller/az_module_manager.py
+++ src/aaz_dev/cli/controller/az_module_manager.py
@@ -36,9 +36,8 @@ class AzModuleManager:
         raise NotImplementedError()
 
     def has_module(self, mod_name):
-        mod_folder = self.get_mod_path(mod_name)
-        if not os.path.exists(mod_folder):
-            #print(f"Invalid module folder: cannot find modules in: '{mod_folder}'")
+        mod_file = os.path.join(self.get_mod_path(mod_name), "__init__.py")
+        if not os.path.exists(mod_file):
             return False
         return True
 
