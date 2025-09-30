1e1270d9d6bf6adb55a304333fd08f190ed99a23
Daniel Languiller <dlanguiller@microsoft.com>
2025-09-30 17:13:46 +1000

    refactor: remove some comments

--- src/web/src/test-setup.ts
+++ src/web/src/test-setup.ts
@@ -4,21 +4,17 @@ import { server } from "./__tests__/mocks/server.js";
 import axios from "axios";
 
 beforeAll(() => {
-  // Set axios base URL for tests
   axios.defaults.baseURL = "http://localhost:3000";
-  
-  // Start MSW server
-  server.listen({ 
-    onUnhandledRequest: "bypass"
+
+  server.listen({
+    onUnhandledRequest: "bypass",
   });
 });
 
 afterEach(() => {
-  // Reset any request handlers that are declared as a part of our tests
   server.resetHandlers();
 });
 
 afterAll(() => {
-  // Clean up once the tests are done
   server.close();
 });
