5470b25d620a03425a6004e3aed14932b3c3eebc
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-07 14:55:36 +1100

    refactor: remove redundant comments from test utils

--- src/web/src/__tests__/test-utils.tsx
+++ src/web/src/__tests__/test-utils.tsx
@@ -4,7 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "../theme";
 
-// Custom render function that includes all providers
 function customRender(ui: ReactElement, options?: Omit<RenderOptions, "wrapper">) {
   function Wrapper({ children }: { children: React.ReactNode }) {
     return (
@@ -17,8 +16,6 @@ function customRender(ui: ReactElement, options?: Omit<RenderOptions, "wrapper">
   return render(ui, { wrapper: Wrapper, ...options });
 }
 
-// Re-export everything from RTL
 export * from "@testing-library/react";
 
-// Override render method with our custom one
 export { customRender as render };
