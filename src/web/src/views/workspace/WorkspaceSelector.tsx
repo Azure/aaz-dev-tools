a4376b2931600736d8af2ad0cf2f2dea129fdc6c
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-14 09:36:28 +1100

    refactor: WorkspaceSelector to function-based

--- src/web/src/views/workspace/WorkspaceSelector.tsx
+++ src/web/src/views/workspace/WorkspaceSelector.tsx
@@ -7,13 +7,6 @@ interface WorkspaceSelectorProps {
   name: string;
 }
 
-interface WorkspaceSelectorState {
-  options: any[];
-  value: WorkspaceType | null;
-  openDialog: boolean;
-  newWorkspaceName: string;
-}
-
 interface InputType {
   inputValue: string;
   title: string;
@@ -21,126 +14,107 @@ interface InputType {
 
 const filter = createFilterOptions<WorkspaceType | InputType>();
 
-class WorkspaceSelector extends React.Component<WorkspaceSelectorProps, WorkspaceSelectorState> {
-  constructor(props: WorkspaceSelectorProps) {
-    super(props);
-    this.state = {
-      options: [],
-      value: null,
-      openDialog: false,
-      newWorkspaceName: "",
-    };
-  }
+const WorkspaceSelector: React.FC<WorkspaceSelectorProps> = ({ name }) => {
+  const [options, setOptions] = React.useState<any[]>([]);
+  const [value, setValue] = React.useState<WorkspaceType | null>(null);
+  const [openDialog, setOpenDialog] = React.useState<boolean>(false);
+  const [newWorkspaceName, setNewWorkspaceName] = React.useState<string>("");
 
-  componentDidMount() {
-    this.loadWorkspaces();
-  }
+  React.useEffect(() => {
+    loadWorkspaces();
+  }, []);
 
-  loadWorkspaces = async () => {
+  const loadWorkspaces = async () => {
     try {
-      const options = await workspaceApi.getWorkspaces();
-      this.setState({
-        options: options,
-      });
+      const workspaces = await workspaceApi.getWorkspaces();
+      setOptions(workspaces);
     } catch (err: any) {
       console.error(err);
     }
   };
 
-  handleDialogClose = (value: any | null) => {
-    this.setState({
-      newWorkspaceName: "",
-      openDialog: false,
-    });
+  const handleDialogClose = (value: any | null) => {
+    setNewWorkspaceName("");
+    setOpenDialog(false);
     if (value != null) {
-      this.onValueUpdated(value);
+      onValueUpdated(value);
     }
   };
 
-  onValueUpdated = (value: any) => {
-    this.setState({
-      value: value,
-    });
+  const onValueUpdated = (value: any) => {
+    setValue(value);
     if (value.url) {
       window.location.href = `/?#/workspace/${value.name}`;
     }
   };
 
-  render() {
-    const { options, value, openDialog, newWorkspaceName } = this.state;
-    const { name } = this.props;
-    return (
-      <React.Fragment>
-        <Autocomplete
-          id="workspace-select"
-          value={value}
-          sx={{ width: 280 }}
-          options={options}
-          autoHighlight
-          onChange={(_event, newValue: any) => {
-            if (typeof newValue === "string") {
-              // timeout to avoid instant validation of the dialog's form.
-              setTimeout(() => {
-                this.setState({
-                  openDialog: true,
-                  newWorkspaceName: newValue,
-                });
-              });
-            } else if (newValue && newValue.inputValue) {
-              this.setState({
-                openDialog: true,
-                newWorkspaceName: newValue.inputValue,
-              });
-            } else {
-              this.onValueUpdated(newValue);
-            }
-          }}
-          filterOptions={(options, params: any) => {
-            const filtered = filter(options, params);
-            if (params.inputValue !== "" && -1 === options.findIndex((e) => e.name === params.inputValue)) {
-              filtered.push({
-                inputValue: params.inputValue,
-                title: `Create "${params.inputValue}"`,
-              });
-            }
-            return filtered;
-          }}
-          getOptionLabel={(option) => {
-            if (typeof option === "string") {
-              return option;
-            }
-            if (option.title) {
-              return option.title;
-            }
-            return option.name;
-          }}
-          renderOption={(props, option) => {
-            const labelName = option && option.title ? option.title : option.name;
-            return (
-              <Box component="li" {...props}>
-                {labelName}
-              </Box>
-            );
-          }}
-          selectOnFocus
-          clearOnBlur
-          renderInput={(params) => (
-            <TextField
-              {...params}
-              label={name}
-              inputProps={{
-                ...params.inputProps,
-                autoComplete: "new-password", // disable autocomplete and autofill
-              }}
-            />
-          )}
-        />
-        {openDialog && (
-          <WorkspaceCreateDialog openDialog={openDialog} onClose={this.handleDialogClose} name={newWorkspaceName} />
+  return (
+    <React.Fragment>
+      <Autocomplete
+        id="workspace-select"
+        value={value}
+        sx={{ width: 280 }}
+        options={options}
+        autoHighlight
+        onChange={(_event, newValue: any) => {
+          if (typeof newValue === "string") {
+            // timeout to avoid instant validation of the dialog's form.
+            setTimeout(() => {
+              setOpenDialog(true);
+              setNewWorkspaceName(newValue);
+            });
+          } else if (newValue && newValue.inputValue) {
+            setOpenDialog(true);
+            setNewWorkspaceName(newValue.inputValue);
+          } else {
+            onValueUpdated(newValue);
+          }
+        }}
+        filterOptions={(options, params: any) => {
+          const filtered = filter(options, params);
+          if (params.inputValue !== "" && -1 === options.findIndex((e) => e.name === params.inputValue)) {
+            filtered.push({
+              inputValue: params.inputValue,
+              title: `Create "${params.inputValue}"`,
+            });
+          }
+          return filtered;
+        }}
+        getOptionLabel={(option) => {
+          if (typeof option === "string") {
+            return option;
+          }
+          if (option.title) {
+            return option.title;
+          }
+          return option.name;
+        }}
+        renderOption={(props, option) => {
+          const labelName = option && option.title ? option.title : option.name;
+          return (
+            <Box component="li" {...props}>
+              {labelName}
+            </Box>
+          );
+        }}
+        selectOnFocus
+        clearOnBlur
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label={name}
+            inputProps={{
+              ...params.inputProps,
+              autoComplete: "new-password", // disable autocomplete and autofill
+            }}
+          />
         )}
-      </React.Fragment>
-    );
-  }
-}
+      />
+      {openDialog && (
+        <WorkspaceCreateDialog openDialog={openDialog} onClose={handleDialogClose} name={newWorkspaceName} />
+      )}
+    </React.Fragment>
+  );
+};
 
 export default WorkspaceSelector;
