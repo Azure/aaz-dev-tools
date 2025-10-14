7d6613c97f0fbcfad12ea383fd14c7740cecd121
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-14 14:24:44 +1100

    refactor : WSEditorExamplePicker

--- src/web/src/views/workspace/WSEditorExamplePicker.tsx
+++ src/web/src/views/workspace/WSEditorExamplePicker.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Box, Autocomplete, TextField } from "@mui/material";
 
 interface ExampleItemsSelectorProps {
@@ -9,49 +9,46 @@ interface ExampleItemsSelectorProps {
   onValueUpdate: (value: string | null) => void;
 }
 
-class ExampleItemSelector extends React.Component<ExampleItemsSelectorProps> {
-  constructor(props: ExampleItemsSelectorProps) {
-    super(props);
-    this.state = {
-      value: this.props.options.length === 1 ? this.props.options[0] : null,
-    };
-  }
+const ExampleItemSelector: React.FC<ExampleItemsSelectorProps> = ({
+  commonPrefix,
+  options,
+  name,
+  value,
+  onValueUpdate,
+}) => {
+  const getOptionLabel = useCallback(
+    (option: string) => {
+      return option.replace(commonPrefix, "");
+    },
+    [commonPrefix],
+  );
 
-  render() {
-    const { name, options, commonPrefix, value } = this.props;
-    return (
-      <Autocomplete
-        id={name}
-        value={value}
-        options={options}
-        onInputChange={(_event, newValue: any) => {
-          this.props.onValueUpdate(newValue);
-        }}
-        getOptionLabel={(option) => {
-          return option.replace(commonPrefix, "");
-        }}
-        renderOption={(props, option) => {
-          return (
-            <Box component="li" {...props}>
-              {option.replace(commonPrefix, "")}
-            </Box>
-          );
-        }}
-        selectOnFocus
-        // clearOnBlur
-        freeSolo
-        renderInput={(params) => (
-          <TextField
-            {...params}
-            size="small"
-            // variant='filled'
-            label={name}
-            required
-          />
-        )}
-      />
-    );
-  }
-}
+  const renderOption = useCallback(
+    (props: any, option: string) => {
+      return (
+        <Box component="li" {...props}>
+          {option.replace(commonPrefix, "")}
+        </Box>
+      );
+    },
+    [commonPrefix],
+  );
+
+  return (
+    <Autocomplete
+      id={name}
+      value={value}
+      options={options}
+      onInputChange={(_event, newValue: any) => {
+        onValueUpdate(newValue);
+      }}
+      getOptionLabel={getOptionLabel}
+      renderOption={renderOption}
+      selectOnFocus
+      freeSolo
+      renderInput={(params) => <TextField {...params} size="small" label={name} required />}
+    />
+  );
+};
 
 export { ExampleItemSelector };
