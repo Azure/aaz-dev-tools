79a4621d2f839575dae2c360e15e34369fc66b93
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-16 13:14:28 +1100

    refactor: WSEditorToolbar to function-based

--- src/web/src/views/workspace/components/WSEditor/WSEditorToolBar.tsx
+++ src/web/src/views/workspace/components/WSEditor/WSEditorToolBar.tsx
@@ -4,7 +4,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { grey } from "@mui/material/colors";
 
-import * as React from "react";
+import React, { Fragment } from "react";
 
 const ArgEditTypography = styled(Typography)<TypographyProps>(() => ({
   color: "#ffffff",
@@ -21,63 +21,66 @@ interface WSEditorToolBarProps {
   onModify: () => void;
 }
 
-class WSEditorToolBar extends React.Component<WSEditorToolBarProps> {
-  render() {
-    const { workspaceName, onHomePage, onGenerate, onDelete, onModify } = this.props;
-    return (
-      <React.Fragment>
-        <AppBar
+const WSEditorToolBar: React.FC<WSEditorToolBarProps> = ({
+  workspaceName,
+  onHomePage,
+  onGenerate,
+  onDelete,
+  onModify,
+}) => {
+  return (
+    <Fragment>
+      <AppBar
+        sx={{
+          position: "fixed",
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+        }}
+      >
+        <Toolbar
           sx={{
-            position: "fixed",
-            zIndex: (theme) => theme.zIndex.drawer + 1,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            height: 64,
           }}
         >
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              height: 64,
-            }}
-          >
-            <IconButton color="inherit" onClick={onHomePage} aria-label="home" sx={{ mr: 2, flexShrink: 0 }}>
-              <HomeIcon sx={{ mr: 2 }} />
-              <Typography variant="h6" component="div" color="inherit" sx={{ mr: 2 }}>
-                WORKSPACE
-              </Typography>
-            </IconButton>
-
-            <Typography variant="h5" component="div" color="inherit">
-              {workspaceName}
+          <IconButton color="inherit" onClick={onHomePage} aria-label="home" sx={{ mr: 2, flexShrink: 0 }}>
+            <HomeIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" component="div" color="inherit" sx={{ mr: 2 }}>
+              WORKSPACE
             </Typography>
-            <Button
-              sx={{ flexShrink: 0, ml: 3 }}
-              startIcon={<EditIcon sx={{ color: grey[100] }} fontSize="small" />}
-              onClick={onModify}
-            >
-              <ArgEditTypography>RENAME</ArgEditTypography>
-            </Button>
-            <Button
-              sx={{ flexShrink: 0, ml: 1 }}
-              startIcon={<DeleteIcon sx={{ color: grey[100] }} fontSize="small" />}
-              onClick={onDelete}
-            >
-              <ArgEditTypography>DELETE</ArgEditTypography>
-            </Button>
+          </IconButton>
 
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ flexShrink: 0 }}>
-              <Tooltip title="Export Command Models">
-                <Button variant="outlined" color="inherit" onClick={onGenerate}>
-                  Export
-                </Button>
-              </Tooltip>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
-    );
-  }
-}
+          <Typography variant="h5" component="div" color="inherit">
+            {workspaceName}
+          </Typography>
+          <Button
+            sx={{ flexShrink: 0, ml: 3 }}
+            startIcon={<EditIcon sx={{ color: grey[100] }} fontSize="small" />}
+            onClick={onModify}
+          >
+            <ArgEditTypography>RENAME</ArgEditTypography>
+          </Button>
+          <Button
+            sx={{ flexShrink: 0, ml: 1 }}
+            startIcon={<DeleteIcon sx={{ color: grey[100] }} fontSize="small" />}
+            onClick={onDelete}
+          >
+            <ArgEditTypography>DELETE</ArgEditTypography>
+          </Button>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexShrink: 0 }}>
+            <Tooltip title="Export Command Models">
+              <Button variant="outlined" color="inherit" onClick={onGenerate}>
+                Export
+              </Button>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </Fragment>
+  );
+};
 
 export default WSEditorToolBar;
