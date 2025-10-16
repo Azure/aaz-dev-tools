c5dc703258497722a65d8f3e54d6b31c8279a7b3
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-16 13:04:08 +1100

    refactor: WSEditorExportDialog to function-based

--- src/web/src/views/workspace/components/WSEditor/WSEditorExportDialog.tsx
+++ src/web/src/views/workspace/components/WSEditor/WSEditorExportDialog.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { Box, Dialog, DialogTitle, DialogContent, DialogActions, LinearProgress, Button, Alert } from "@mui/material";
 import { workspaceApi, errorHandlerApi } from "../../../../services";
 
@@ -9,117 +9,102 @@ interface WSEditorExportDialogProps {
   onClose: (exported: boolean, showClientConfigDialog: boolean) => void;
 }
 
-interface WSEditorExportDialogState {
-  updating: boolean;
-  invalidText: string | undefined;
-  clientConfigOOD: boolean;
-}
+const WSEditorExportDialog: React.FC<WSEditorExportDialogProps> = ({
+  workspaceUrl,
+  open,
+  clientConfigurable,
+  onClose,
+}) => {
+  const [updating, setUpdating] = useState<boolean>(false);
+  const [invalidText, setInvalidText] = useState<string | undefined>(undefined);
+  const [clientConfigOOD, setClientConfigOOD] = useState<boolean>(false);
 
-class WSEditorExportDialog extends React.Component<WSEditorExportDialogProps, WSEditorExportDialogState> {
-  constructor(props: WSEditorExportDialogProps) {
-    super(props);
-    this.state = {
-      updating: false,
-      invalidText: undefined,
-      clientConfigOOD: false,
-    };
-  }
-
-  componentDidMount(): void {
-    if (this.props.clientConfigurable) {
-      this.verifyClientConfig();
+  useEffect(() => {
+    if (clientConfigurable) {
+      verifyClientConfig();
     }
-  }
+  }, [clientConfigurable]);
 
-  handleClose = () => {
-    this.props.onClose(false, false);
+  const handleClose = () => {
+    onClose(false, false);
   };
 
-  verifyClientConfig = async () => {
-    this.setState({ updating: true });
+  const verifyClientConfig = async () => {
+    setUpdating(true);
     try {
-      await workspaceApi.verifyClientConfig(this.props.workspaceUrl);
-      this.setState({ clientConfigOOD: false, updating: false });
+      await workspaceApi.verifyClientConfig(workspaceUrl);
+      setClientConfigOOD(false);
+      setUpdating(false);
     } catch (err: any) {
       // catch 409 error
       if (errorHandlerApi.isHttpError(err, 409)) {
-        this.setState({
-          invalidText: `The client config in this workspace is out of date. Please refresh it first.`,
-          clientConfigOOD: true,
-          updating: false,
-        });
+        setInvalidText(`The client config in this workspace is out of date. Please refresh it first.`);
+        setClientConfigOOD(true);
+        setUpdating(false);
         return;
       } else {
         console.error(err);
-        this.setState({
-          invalidText: errorHandlerApi.getErrorMessage(err),
-          updating: false,
-        });
+        setInvalidText(errorHandlerApi.getErrorMessage(err));
+        setUpdating(false);
       }
     }
   };
 
-  inheritClientConfig = async () => {
-    this.setState({ updating: true });
+  const inheritClientConfig = async () => {
+    setUpdating(true);
     try {
-      await workspaceApi.inheritClientConfig(this.props.workspaceUrl);
-      this.setState({ clientConfigOOD: false, updating: false });
-      this.props.onClose(false, true);
+      await workspaceApi.inheritClientConfig(workspaceUrl);
+      setClientConfigOOD(false);
+      setUpdating(false);
+      onClose(false, true);
     } catch (err: any) {
       console.error(err);
-      this.setState({
-        invalidText: errorHandlerApi.getErrorMessage(err),
-        updating: false,
-      });
+      setInvalidText(errorHandlerApi.getErrorMessage(err));
+      setUpdating(false);
     }
   };
 
-  handleExport = async () => {
-    this.setState({ updating: true });
+  const handleExport = async () => {
+    setUpdating(true);
 
     try {
-      await workspaceApi.generateWorkspace(this.props.workspaceUrl);
-      this.setState({ updating: false });
-      this.props.onClose(false, false);
+      await workspaceApi.generateWorkspace(workspaceUrl);
+      setUpdating(false);
+      onClose(false, false);
     } catch (err: any) {
       console.error(err);
-      this.setState({
-        invalidText: errorHandlerApi.getErrorMessage(err),
-        updating: false,
-      });
+      setInvalidText(errorHandlerApi.getErrorMessage(err));
+      setUpdating(false);
     }
   };
 
-  render(): React.ReactNode {
-    const { updating, invalidText, clientConfigOOD } = this.state;
-    return (
-      <Dialog disableEscapeKeyDown open={this.props.open}>
-        <DialogTitle>Export workspace command models to AAZ Repo</DialogTitle>
-        <DialogContent>
-          {invalidText && (
-            <Alert variant="filled" severity="error">
-              {" "}
-              {invalidText}{" "}
-            </Alert>
-          )}
-        </DialogContent>
-        <DialogActions>
-          {updating && (
-            <Box sx={{ width: "100%" }}>
-              <LinearProgress color="secondary" />
-            </Box>
-          )}
-          {!updating && (
-            <React.Fragment>
-              {clientConfigOOD && <Button onClick={this.inheritClientConfig}>Refresh Client Config</Button>}
-              {!clientConfigOOD && <Button onClick={this.handleClose}>Cancel</Button>}
-              {!clientConfigOOD && <Button onClick={this.handleExport}>Confirm</Button>}
-            </React.Fragment>
-          )}
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
+  return (
+    <Dialog disableEscapeKeyDown open={open}>
+      <DialogTitle>Export workspace command models to AAZ Repo</DialogTitle>
+      <DialogContent>
+        {invalidText && (
+          <Alert variant="filled" severity="error">
+            {" "}
+            {invalidText}{" "}
+          </Alert>
+        )}
+      </DialogContent>
+      <DialogActions>
+        {updating && (
+          <Box sx={{ width: "100%" }}>
+            <LinearProgress color="secondary" />
+          </Box>
+        )}
+        {!updating && (
+          <Fragment>
+            {clientConfigOOD && <Button onClick={inheritClientConfig}>Refresh Client Config</Button>}
+            {!clientConfigOOD && <Button onClick={handleClose}>Cancel</Button>}
+            {!clientConfigOOD && <Button onClick={handleExport}>Confirm</Button>}
+          </Fragment>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default WSEditorExportDialog;
