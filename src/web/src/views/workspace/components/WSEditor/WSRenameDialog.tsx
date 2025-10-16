3a9c7606cecaa466feebd74cf32d77fd06e97bd7
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-16 13:22:42 +1100

    refactor: WSRenameDialog to function-based

--- src/web/src/views/workspace/components/WSEditor/WSRenameDialog.tsx
+++ src/web/src/views/workspace/components/WSEditor/WSRenameDialog.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, Fragment } from "react";
 import {
   Box,
   Dialog,
@@ -19,114 +19,82 @@ interface WSRenameDialogProps {
   onClose: (newWSName: string | null) => void;
 }
 
-interface WSRenameDialogState {
-  newWSName: string;
-  invalidText?: string;
-  updating: boolean;
-}
+const WSRenameDialog: React.FC<WSRenameDialogProps> = ({ workspaceUrl, workspaceName, open, onClose }) => {
+  const [newWSName, setNewWSName] = useState<string>(workspaceName);
+  const [invalidText, setInvalidText] = useState<string | undefined>(undefined);
+  const [updating, setUpdating] = useState<boolean>(false);
 
-class WSRenameDialog extends React.Component<WSRenameDialogProps, WSRenameDialogState> {
-  constructor(props: WSRenameDialogProps) {
-    super(props);
-    this.state = {
-      newWSName: this.props.workspaceName,
-      updating: false,
-    };
-  }
-
-  handleModify = () => {
-    const { newWSName } = this.state;
-    const { workspaceUrl, workspaceName } = this.props;
-
+  const handleModify = () => {
     const nName = newWSName.trim();
     if (nName.length < 1) {
-      this.setState({
-        invalidText: `Field 'Name' is required.`,
-      });
+      setInvalidText(`Field 'Name' is required.`);
       return;
     }
 
-    this.setState({
-      invalidText: undefined,
-    });
-    this.setState({
-      updating: true,
-    });
+    setInvalidText(undefined);
+    setUpdating(true);
 
     if (workspaceName === nName) {
-      this.setState({
-        updating: false,
-      });
-      this.props.onClose(null);
+      setUpdating(false);
+      onClose(null);
     } else {
       workspaceApi
         .renameWorkspace(workspaceUrl, nName)
         .then((res: any) => {
-          this.setState({
-            updating: false,
-          });
-          this.props.onClose(res.name);
+          setUpdating(false);
+          onClose(res.name);
         })
         .catch((err: any) => {
-          this.setState({
-            updating: false,
-            invalidText: errorHandlerApi.getErrorMessage(err),
-          });
+          setUpdating(false);
+          setInvalidText(errorHandlerApi.getErrorMessage(err));
         });
     }
   };
 
-  handleClose = () => {
-    this.setState({
-      invalidText: undefined,
-    });
-    this.props.onClose(null);
+  const handleClose = () => {
+    setInvalidText(undefined);
+    onClose(null);
   };
 
-  render() {
-    const { invalidText, updating } = this.state;
-    return (
-      <Dialog disableEscapeKeyDown open={this.props.open} sx={{ "& .MuiDialog-paper": { width: "80%" } }}>
-        <DialogTitle>Rename Workspace</DialogTitle>
-        <DialogContent dividers={true}>
-          {invalidText && (
-            <Alert variant="filled" severity="error">
-              {" "}
-              {invalidText}{" "}
-            </Alert>
-          )}
-          <TextField
-            id="name"
-            label="Name"
-            type="text"
-            fullWidth
-            variant="standard"
-            value={this.state.newWSName}
-            onChange={(event: any) => {
-              this.setState({
-                newWSName: event.target.value,
-              });
-            }}
-            margin="normal"
-            required
-          />
-        </DialogContent>
-        <DialogActions>
-          {updating && (
-            <Box sx={{ width: "100%" }}>
-              <LinearProgress color="secondary" />
-            </Box>
-          )}
-          {!updating && (
-            <React.Fragment>
-              <Button onClick={this.handleClose}>Cancel</Button>
-              <Button onClick={this.handleModify}>Save</Button>
-            </React.Fragment>
-          )}
-        </DialogActions>
-      </Dialog>
-    );
-  }
-}
+  return (
+    <Dialog disableEscapeKeyDown open={open} sx={{ "& .MuiDialog-paper": { width: "80%" } }}>
+      <DialogTitle>Rename Workspace</DialogTitle>
+      <DialogContent dividers={true}>
+        {invalidText && (
+          <Alert variant="filled" severity="error">
+            {" "}
+            {invalidText}{" "}
+          </Alert>
+        )}
+        <TextField
+          id="name"
+          label="Name"
+          type="text"
+          fullWidth
+          variant="standard"
+          value={newWSName}
+          onChange={(event: any) => {
+            setNewWSName(event.target.value);
+          }}
+          margin="normal"
+          required
+        />
+      </DialogContent>
+      <DialogActions>
+        {updating && (
+          <Box sx={{ width: "100%" }}>
+            <LinearProgress color="secondary" />
+          </Box>
+        )}
+        {!updating && (
+          <Fragment>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleModify}>Save</Button>
+          </Fragment>
+        )}
+      </DialogActions>
+    </Dialog>
+  );
+};
 
 export default WSRenameDialog;
