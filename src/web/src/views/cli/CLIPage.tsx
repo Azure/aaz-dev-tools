373abe7fcb134344abfae6125e2de51257ec6512
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-18 15:17:39 +1100

    refactor: CLIPage to function-based

--- src/web/src/views/cli/CLIPage.tsx
+++ src/web/src/views/cli/CLIPage.tsx
@@ -1,14 +1,12 @@
-import * as React from "react";
+import React from "react";
 import { Outlet } from "react-router";
 
-class CLIPage extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <Outlet />
-      </React.Fragment>
-    );
-  }
-}
+const CLIPage: React.FC = () => {
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+};
 
 export default CLIPage;
