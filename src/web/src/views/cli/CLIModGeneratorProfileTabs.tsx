6274d172dee5703f3b0c560d812c396f8dcee645
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-18 15:04:04 +1100

    refactor: CLIModGeneratorProfileTabs

--- src/web/src/views/cli/CLIModGeneratorProfileTabs.tsx
+++ src/web/src/views/cli/CLIModGeneratorProfileTabs.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 
@@ -8,34 +8,29 @@ interface CLIModGeneratorProfileTabsProps {
   onChange: (newValue: string) => void;
 }
 
-class CLIModGeneratorProfileTabs extends React.Component<CLIModGeneratorProfileTabsProps> {
-  render() {
-    const { value, profiles, onChange } = this.props;
-    return (
-      <Tabs
-        orientation="vertical"
-        variant="scrollable"
-        value={value}
-        onChange={(_event, newValue) => {
-          onChange(newValue);
-        }}
-        aria-label="Vertical tabs example"
-        sx={{ borderRight: 1, borderColor: "divider" }}
-      >
-        {profiles.map((profile, _idx) => {
-          return (
-            <Tab
-              key={profile}
-              label={profile}
-              id={`vertical-tab-${profile}`}
-              aria-controls={`vertical-tabpanel-${profile}`}
-              value={profile}
-            />
-          );
-        })}
-      </Tabs>
-    );
-  }
-}
+const CLIModGeneratorProfileTabs: React.FC<CLIModGeneratorProfileTabsProps> = ({ value, profiles, onChange }) => {
+  return (
+    <Tabs
+      orientation="vertical"
+      variant="scrollable"
+      value={value}
+      onChange={(_event, newValue) => {
+        onChange(newValue);
+      }}
+      aria-label="Vertical tabs example"
+      sx={{ borderRight: 1, borderColor: "divider" }}
+    >
+      {profiles.map((profile) => (
+        <Tab
+          key={profile}
+          label={profile}
+          id={`vertical-tab-${profile}`}
+          aria-controls={`vertical-tabpanel-${profile}`}
+          value={profile}
+        />
+      ))}
+    </Tabs>
+  );
+};
 
 export default CLIModGeneratorProfileTabs;
