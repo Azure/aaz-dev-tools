7da9762421da1267daf4a928b3316a0123300339
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-18 14:05:38 +1100

    refactor: AppNavBar to function-based

--- src/web/src/components/AppNavBar.tsx
+++ src/web/src/components/AppNavBar.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import Link from "@mui/material/Link";
 import { AppBar, Toolbar } from "@mui/material";
@@ -11,110 +11,91 @@ type AppNavBarProps = {
   pageName: string | null;
 };
 
-type AppNavBarState = {
-  anchorEl: null | HTMLElement;
-};
+const AppNavBar: React.FC<AppNavBarProps> = ({ pageName }) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
 
-class AppNavBar extends React.Component<AppNavBarProps, AppNavBarState> {
-  constructor(props: AppNavBarProps) {
-    super(props);
-    this.state = {
-      anchorEl: null,
-    };
-  }
-
-  handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-    this.setState({ anchorEl: event.currentTarget });
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
   };
 
-  handleMenuClose = () => {
-    this.setState({ anchorEl: null });
+  const handleMenuClose = () => {
+    setAnchorEl(null);
   };
 
-  render() {
-    const { anchorEl } = this.state;
-
-    return (
-      <div>
-        <AppBar position="fixed">
-          <Toolbar sx={{ justifyContent: "space-between", height: 64 }}>
-            <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
-              <Link
-                variant="h6"
-                underline="none"
-                color="inherit"
-                href="/?#/HomePage"
-                fontWeight={
-                  this.props.pageName === "HomePage"
-                    ? theme.typography.fontWeightMedium
-                    : theme.typography.fontWeightLight
-                }
-              >
-                {"Home"}
-              </Link>
-              <Box sx={{ p: 4 }} />
-              <Link
-                variant="h6"
-                underline="none"
-                color="inherit"
-                href="/?#/Workspace"
-                fontWeight={
-                  this.props.pageName === "Workspace"
-                    ? theme.typography.fontWeightMedium
-                    : theme.typography.fontWeightLight
-                }
-              >
-                {"Workspace"}
-              </Link>
-              <Box sx={{ p: 4 }} />
-              <Link
-                variant="h6"
-                underline="none"
-                color="inherit"
-                href="/?#/CLI"
-                fontWeight={
-                  this.props.pageName === "CLI" ? theme.typography.fontWeightMedium : theme.typography.fontWeightLight
-                }
-              >
-                {"CLI"}
-              </Link>
-            </Box>
-
-            <Button
+  return (
+    <div>
+      <AppBar position="fixed">
+        <Toolbar sx={{ justifyContent: "space-between", height: 64 }}>
+          <Box sx={{ flex: 1, display: "flex", justifyContent: "flex-start", alignItems: "center" }}>
+            <Link
+              variant="h6"
+              underline="none"
               color="inherit"
-              sx={{
-                fontSize: "18px",
-                fontFamily: "Roboto Condensed, sans-serif",
-              }}
-              onClick={this.handleMenuOpen}
+              href="/?#/HomePage"
+              fontWeight={
+                pageName === "HomePage" ? theme.typography.fontWeightMedium : theme.typography.fontWeightLight
+              }
             >
-              Help
-            </Button>
-            <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleMenuClose}>
-              <MenuItem
-                onClick={this.handleMenuClose}
-                component="a"
-                href="https://azure.github.io/aaz-dev-tools/"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Document
-              </MenuItem>
-              <MenuItem
-                onClick={this.handleMenuClose}
-                component="a"
-                href="https://forms.office.com/r/j6rQuFUqUf?origin=lprLink"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Send a Feedback
-              </MenuItem>
-            </Menu>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
-  }
-}
+              Home
+            </Link>
+            <Box sx={{ p: 4 }} />
+            <Link
+              variant="h6"
+              underline="none"
+              color="inherit"
+              href="/?#/Workspace"
+              fontWeight={
+                pageName === "Workspace" ? theme.typography.fontWeightMedium : theme.typography.fontWeightLight
+              }
+            >
+              Workspace
+            </Link>
+            <Box sx={{ p: 4 }} />
+            <Link
+              variant="h6"
+              underline="none"
+              color="inherit"
+              href="/?#/CLI"
+              fontWeight={pageName === "CLI" ? theme.typography.fontWeightMedium : theme.typography.fontWeightLight}
+            >
+              CLI
+            </Link>
+          </Box>
+
+          <Button
+            color="inherit"
+            sx={{
+              fontSize: "18px",
+              fontFamily: "Roboto Condensed, sans-serif",
+            }}
+            onClick={handleMenuOpen}
+          >
+            Help
+          </Button>
+          <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+            <MenuItem
+              onClick={handleMenuClose}
+              component="a"
+              href="https://azure.github.io/aaz-dev-tools/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Document
+            </MenuItem>
+            <MenuItem
+              onClick={handleMenuClose}
+              component="a"
+              href="https://forms.office.com/r/j6rQuFUqUf?origin=lprLink"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Send a Feedback
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 export { AppNavBar };
