e7bc5e68f824c5f2c2573d0b97e6cad1c9bf6042
Daniel Languiller <dlanguiller@microsoft.com>
2025-10-18 15:10:12 +1100

    Refactor: CLIModGeneratorToolBar to function based

--- src/web/src/views/cli/CLIModGeneratorToolBar.tsx
+++ src/web/src/views/cli/CLIModGeneratorToolBar.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { AppBar, Button, IconButton, Toolbar, Typography, Tooltip, Box } from "@mui/material";
 import HomeIcon from "@mui/icons-material/Home";
 
@@ -8,44 +8,41 @@ interface CLIModGeneratorToolBarProps {
   onGenerate: () => void;
 }
 
-class CLIModGeneratorToolBar extends React.Component<CLIModGeneratorToolBarProps> {
-  render() {
-    const { moduleName, onHomePage, onGenerate } = this.props;
-    return (
-      <React.Fragment>
-        <AppBar sx={{ position: "fixed", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start",
-              height: 64,
-            }}
-          >
-            <IconButton color="inherit" onClick={onHomePage} aria-label="home" sx={{ mr: 2, flexShrink: 0 }}>
-              <HomeIcon sx={{ mr: 2 }} />
-              <Typography variant="h6" component="div" color="inherit" sx={{ mr: 2 }}>
-                GENERATION
-              </Typography>
-            </IconButton>
-
-            <Typography variant="h5" component="div" color="inherit">
-              {moduleName}
+const CLIModGeneratorToolBar: React.FC<CLIModGeneratorToolBarProps> = ({ moduleName, onHomePage, onGenerate }) => {
+  return (
+    <>
+      <AppBar sx={{ position: "fixed", zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-start",
+            height: 64,
+          }}
+        >
+          <IconButton color="inherit" onClick={onHomePage} aria-label="home" sx={{ mr: 2, flexShrink: 0 }}>
+            <HomeIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" component="div" color="inherit" sx={{ mr: 2 }}>
+              GENERATION
             </Typography>
+          </IconButton>
 
-            <Box sx={{ flexGrow: 1 }} />
-            <Box sx={{ flexShrink: 0 }}>
-              <Tooltip title="Generate CLI Commands">
-                <Button variant="outlined" color="inherit" onClick={onGenerate}>
-                  Generate
-                </Button>
-              </Tooltip>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </React.Fragment>
-    );
-  }
-}
+          <Typography variant="h5" component="div" color="inherit">
+            {moduleName}
+          </Typography>
+
+          <Box sx={{ flexGrow: 1 }} />
+          <Box sx={{ flexShrink: 0 }}>
+            <Tooltip title="Generate CLI Commands">
+              <Button variant="outlined" color="inherit" onClick={onGenerate}>
+                Generate
+              </Button>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </AppBar>
+    </>
+  );
+};
 
 export default CLIModGeneratorToolBar;
